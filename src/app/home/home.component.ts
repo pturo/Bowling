3356300107499b3da76eba8cafe107814b03b4d9
Paweł Turoń <34440020+pturo@users.noreply.github.com>
2021-06-16 19:42:24 +0200

    Work on reading txt file
    Work on reading txt file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,45 +16,46 @@ export class HomeComponent implements OnInit {
     let fileReader: FileReader = new FileReader();
     let self = this;
     fileReader.onloadend = function () {
-      //self.fileContent = fileReader.result as string;
-      // read line by line and split by commas
-      var line = (<string>fileReader.result).split('/[\r\n]+/');
-      for(var i = 0; i < line.length; i++){
-        console.log("Line: " + i, line[i]);
-      }
-      var comma = (<string>fileReader.result).split(',');
-      for(var i = 0; i < comma.length; i++){
-        console.log("Comma: " + i, comma[i]);
-      }
-      self.results = [
+      // read line by line
+      for (const line of (<string>fileReader.result).split(/[\r\n]+/)){
+        //console.log("line: " + line);
+
+        for(const k of line.split(","))
         {
-          player: comma[i],
-          totalScore: 12,
-          one: Number(comma[i]),
-          two: Number(comma[i]),
-          three: Number(comma[i]),
-          four: Number(comma[i]),
-          five: Number(comma[i]),
-          six: Number(comma[i]),
-          seven: Number(comma[i]),
-          eight: Number(comma[i]),
-          nine: Number(comma[i]),
-          ten: Number(comma[i]),
-          eleven: Number(comma[i]),
-          twelve: Number(comma[i]),
-          thirteen: Number(comma[i]),
-          fourteen: Number(comma[i]),
-          fifteen: Number(comma[i]),
-          sixteen: Number(comma[i]),
-          seventeen: Number(comma[i]),
-          eighteen: Number(comma[i]),
-          nineteen: Number(comma[i]),
-          twenty: Number(comma[i]),
-          twentyOne: Number(comma[i]),
-          twentyTwo: Number(comma[i]),
+          console.log("k: " + k);
+
+          self.results = [
+            {
+              player: k,
+              totalScore: 12435,
+              one: Number(k),
+              two: Number(k),
+              three: Number(k),
+              four: Number(k),
+              five: Number(k),
+              six: Number(k),
+              seven: Number(k),
+              eight: Number(k),
+              nine: Number(k),
+              ten: Number(k),
+              eleven: Number(k),
+              twelve: Number(k),
+              thirteen: Number(k),
+              fourteen: Number(k),
+              fifteen: Number(k),
+              sixteen: Number(k),
+              seventeen: Number(k),
+              eighteen: Number(k),
+              nineteen: Number(k),
+              twenty: Number(k),
+              twentyOne: Number(k),
+              twentyTwo: Number(k),
+            }
+          ];
+
+          console.log(self.results);
         }
-      ];
-      console.log(self.results);
+      }
     }
     fileReader.readAsText(file);
   }
