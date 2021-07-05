93a873079c0e42928bfdf1ae53cd9e7420ad8f72
Paweł Turoń <34440020+pturo@users.noreply.github.com>
2021-07-05 13:31:53 +0200

    Update sum
    Update sum

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,71 +8,84 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
 
-  res: any;
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // With JSON
-  // public loadData(event: any) {
-  //   let file = event.target.files[0];
-  //   let self = this; // for accessing jsonResults variable
-  //   if(file) {
-  //     let fileReader: FileReader = new FileReader();
-  //     fileReader.readAsText(file, "utf-8");
-  //     fileReader.onload = function(evt: any) {
-  //       self.jsonResults = JSON.parse(evt.target["result"]);
-  //       // DEBUG
-  //       // for(var a = 0; a < self.jsonResults.length; a++) {
-  //       //   console.log("data: " + JSON.stringify(self.jsonResults[a].score));
-  //       // }
-  //       // console.log("length: " + self.jsonResults.length);
-  //       for(var i = 0; i < self.jsonResults.length; i++) {
-  //         self.jsonResults[i].totalScore = Number(JSON.stringify(self.jsonResults[i].score.reduce((a: any, b: any) => a + b, 0)));
-  //         // DEBUG
-  //         console.log("wynik: " + Number(JSON.stringify(self.jsonResults[i].totalScore)));
-  //       }
-  //     }
-  //   }
-  // }
-
-  // different approach
-  // WARNING: these lines of code were made for
-  // testing reading content from .txt files and
-  // they are not valid anymore. Our task uses JSON format.
+  // Load contents from .txt file
   public loadData(event: any) {
-    var self = this;
+    // clear all records before initializing data from txt file
+    $("#tBodyClear").empty();
+
+    var score: any;
     let file = event.target.files[0];
     let fileReader: FileReader = new FileReader();
     fileReader.onloadend = function () {
-      let rows = (<string>fileReader.result).toString().replace(/[\r\n]+/gm, ",").split("\n");
-      console.log("rows: " + rows);
-      // get each row of txt file
-      for (var i = 0; i < rows.length; i++) {
-        var validRowData = [];
-        var rowData = rows[i].split(",");
-        console.log("rowData: " + rowData);
-        // get data from row
-        for(var j = 0; j < rowData.length; j++) {
-          if (rowData[j] == "") {
-          } else {
-            validRowData.push(rowData[j]);
-          }
+      const tbody: any = document.querySelector("tbody");
+      const rows = (<string>fileReader.result).replace(/[\r\n]+/gm, "\n").split(/^/m).reduce((acc: any, row: any, i) => {
+        row = row.trim();
+
+        if (/^\d/.test(row)) {
+          //console.log("???", row);
+          acc[acc.length - 1].push(...row.split(/,\s?/));
+        } else {
+          acc.push([row]);
         }
 
-        // create row template
-        var rowTemplate = "<tr>";
-        for (var k = 0; k < validRowData.length; k++) {
-          rowTemplate += "<td>" + validRowData[k] + "</td>";
-          //console.log("k: " + k + " validRowData[" + k + "]: " + validRowData[k]);
-        }
-        rowTemplate += "</tr>";
-        // append to table
-        $('#resultsTable').append(rowTemplate);
-      }
+        return acc;
+      }, []);
+
+      //console.log("rows:", rows);
+
+      rows.forEach((cells: any) => {
+        const tr = document.createElement("tr");
+
+        cells.forEach((cell: any) => {
+          const td = document.createElement("td");
+          td.textContent = cell;
+
+          tr.appendChild(td);
+        });
+
+        // sum all points
+        score = cells.map(function (elt: any) { // assure the value can be converted into an integer
+          return /^\d+$/.test(elt) ? parseInt(elt) : 0;
+        }).reduce(function (a: number, b: number) { // sum all resulting numbers
+          return a + b
+        });
+        //console.log("score[cells]:" + score);
+
+        const totalScore = document.createElement("td");
+        totalScore.textContent = score;
+        tr.appendChild(totalScore);
+
+        tbody.appendChild(tr);
+      });
+
+      normalizeTable();
     }
     fileReader.readAsText(file);
   }
 }
+
+function normalizeTable() {
+  // align everything
+  var table = document.querySelector("table");
+  var trs = table!.getElementsByTagName("tr"), len = trs.length, max = 0, td;
+  // search for the longest table row in terms of # of children
+  for (var i = len; i--;) {
+    if (trs[i].children.length > max)
+      max = trs[i].children.length;
+      console.log("max: " + max);
+  }
+  // now fill the other rows
+  for (var j = len; j--;) {
+    while (trs[j].children.length < max) {
+      td = document.createElement("td");
+      trs[j].appendChild(td);
+      console.log("trsappend: " + trs[j].appendChild(td));
+    }
+  }
+}
+
